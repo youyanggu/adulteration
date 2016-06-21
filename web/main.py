36494ac766bf900d9fb6145468a40449db24ec5f
Youyang Gu <youyanggu@gmail.com>
2016-06-21 14:20:13 -0400

    updated web tool

--- web/main.py
+++ web/main.py
@@ -24,6 +24,7 @@ from nn import NN
 app = Flask(__name__)
 
 print "Loading data. Will take 1 minute."
+HIER_DIMENSION = 4290 # dimension of the input vector to the neural network
 sources = ['SNOMEDCT_US', 'NCI', 'NDFRT', 'MSH'] # use data from these 4 sources
 df_conso = pd.read_hdf('mrconso.h5', 'mrconso')
 df_hier = pd.read_hdf('mrhier.h5', 'mrhier')
@@ -32,67 +33,55 @@ with open('cuis_to_idx.pkl', 'r') as f:
 with open('idx_to_cat.pkl', 'rb') as f:
     idx_to_cat = pickle.load(f)
 categories = [idx_to_cat[i] for i in range(len(idx_to_cat))]
+aui_to_cui = df_conso.set_index('AUI')['CUI'].to_dict()
+aui_to_str = df_conso.set_index('AUI')['STR'].to_dict()
 print "Done loading data"
 
-def get_ing_to_hiers(cui, df_hier, sources):
-    """Get the hierarchy for CUI in the form of AUIs.
-    AUIs are a more specific form of CUIs, which we will not use.
-    """
+def get_cui_to_hiers(cui):
+    """Look up the hierarchy for CUI in the form of AUIs."""
     df_hier_short = df_hier[df_hier['CUI']==cui]
     hiers = df_hier_short[df_hier_short['SAB'].isin(sources)]
-    sources_count = {}
     if len(hiers) == 0:
-        print "No hierarchy for:", cui
         return []
-    
-    for i in np.unique(hiers['SAB']):
-        if i not in sources_count:
-            sources_count[i] = 1
-        else:
-            sources_count[i] += 1
     cur_auis = hiers['AUI'].values
     rows = hiers['PTR'].str.split('.').values
-    auis = []
+    hiers_aui = []
     for aui, r in zip(cur_auis, rows):
-        auis.append([j for j in r] + [aui])
-    return auis
+        hiers_aui.append([j for j in r] + [aui])
+    return hiers_aui
 
-def convert_auis_to_cuis(auis, df):
-    """Convert the hierarchy from AUIs to CUIs."""
-    aui_to_cui = df.set_index('AUI')['CUI'].to_dict()
+def convert_auis(hiers_aui):
+    """Convert the hierarchy from AUIs to CUIs and strings."""
+    if len(hiers_aui) == 0:
+        return [], [], {}
     cuis = []
-    for path in auis:
-        cuis.append([aui_to_cui[aui] for aui in path])
-    return cuis
-
-def convert_hier_to_str(auis, df):
-    """Convert the hierarchy from AUIs to human understandable format."""
-    aui_to_str = df.set_index('AUI')['STR'].to_dict()
     strs = []
-    for path in auis:
+    cui_to_str = {}
+    for path in hiers_aui:
+        cuis.append([aui_to_cui[aui] for aui in path])
         strs.append([aui_to_str[aui] for aui in path])
-    return strs
-
-def convert_hiers_to_rep(hiers, cuis_to_idx):
-    """Convert hierarchy into a vector representation."""
-    vectors = []
-    for path in hiers:
-        v = np.zeros(len(cuis_to_idx))
-        for cui in path:
-            if cui in cuis_to_idx:
-                v[cuis_to_idx[cui]] = 1
-        vectors.append(v)
-    rep = np.mean(np.array(vectors), axis=0)
-    return rep.astype('float32')
+        for aui in path:
+           cui_to_str[aui_to_cui[aui]] = aui_to_str[aui]
+    return cuis, strs, cui_to_str
 
 def get_hiers_from_cui(cui):
     """Runs pipeline that converts a CUI to a hierarchy."""
-    hiers_aui = get_ing_to_hiers(cui, df_hier, sources)
-    if len(hiers_aui) == 0:
-        return [], []
-    hiers = convert_auis_to_cuis(hiers_aui, df_conso)
-    hiers_str = convert_hier_to_str(hiers_aui, df_conso)
-    return hiers, hiers_str
+    hiers_aui = get_cui_to_hiers(cui)
+    return convert_auis(hiers_aui)
+
+def convert_hiers_to_rep(hiers, cuis_to_idx, cuis_to_str):
+    """Convert hierarchy into a vector representation."""
+    rep_str = set()
+    vectors = []
+    for path in hiers:
+        v = np.zeros(HIER_DIMENSION)
+        for cui in path:
+            if cui in cuis_to_idx:
+                rep_str.add(cuis_to_str[cui])
+                v[cuis_to_idx[cui]] = 1
+        vectors.append(v)
+    rep = np.mean(np.array(vectors), axis=0)
+    return rep.astype('float32'), sorted(rep_str)
 
 def load_model():
     """Load the pre-trained neural network model."""
@@ -103,7 +92,7 @@ def load_model():
     x = T.fmatrix('x')
     classifier = NN(
         inp=x,
-        n_in=4290,             # length of hierarchy representation
+        n_in=HIER_DIMENSION,   # length of hierarchy representation
         m=100,                 # number of hidden layers
         n_out=len(categories), # number of outputs
         W_hid=W_hid,
@@ -122,7 +111,8 @@ def predict(predict_model, rep):
     neural network."""
     rep = rep.reshape(1,-1)
     result = predict_model(rep)[0]
-    category_to_score = [(cat.capitalize(), score) for cat, score in zip(categories, result)]
+    category_to_score = [(cat.capitalize(), score) for cat, score in zip(
+        categories, result)]
     category_to_score = sorted(category_to_score, key=lambda x:x[1], reverse=True)
     return category_to_score[:20]
 
@@ -141,19 +131,26 @@ def show_main():
 @app.route('/show', methods=['POST'])
 def show_post():
     cui = request.form['cui']
+    if len(cui) != 8 or not cui.startswith('C'):
+        ret_str = 'CUI must start with C and contain exactly 8 characters/digits'
+        return render_template('main.html', result=None, ret_str=ret_str, cui=cui)
     result = df_conso[df_conso['CUI']==cui]
     if len(result) == 0:
         ret_str = 'CUI not found'
         return render_template('main.html', result=None, ret_str=ret_str, cui=cui)
-    hiers, hiers_str = get_hiers_from_cui(cui)
+    hiers, hiers_str, cuis_to_str = get_hiers_from_cui(cui)
     if len(hiers) == 0:
-        ret_str = ('A match is found, but either 1) no hierarchy exists or'
+        ret_str = ('A match is found, but either 1) no hierarchy exists or '
         '2) the hierarchy does not exist in our database')
         return render_template('main.html', result=None, ret_str=ret_str, cui=cui)
-    rep = convert_hiers_to_rep(hiers, cuis_to_idx)
+    rep, rep_str = convert_hiers_to_rep(hiers, cuis_to_idx, cuis_to_str)
     num_nodes = (rep>0).sum()
+    if num_nodes == 0:
+        ret_str = ('The model is unable to make a prediction from this '
+            'hierarchy due to lack of data:')
+        return render_template('main.html', result=None, ret_str=ret_str, cui=cui)
     predict_model = load_model()
     category_to_score = predict(predict_model, rep)
     return render_template('main.html', result=hiers_str, cui=cui, 
-        category_to_score=category_to_score, num_nodes=num_nodes)
+        category_to_score=category_to_score, num_nodes=num_nodes, rep_str=rep_str)
 
